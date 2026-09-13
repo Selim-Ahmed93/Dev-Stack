054d1951335fab00c70181853a452352a2851510
SELIM-AHMED- <mdselimahmed6467@gmail.com>
2026-09-13 09:48:43 +0600

    Footer section Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Components/Footer";
 import Hero from "./Components/Hero";
 import Navbar from "./Components/Navbar";
 import TechStack from "./Components/TechStack";
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <TechStack />
+      <Footer/>
     </>
   );
 }
